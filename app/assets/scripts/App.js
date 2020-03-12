580ae8819ac5018364c7dae72c82763502d15a0d
Ahmed <ahmednhosna@gmail.com>
2020-03-12 07:38:48 +0200

    Finished header styles, and creating a show/hide toggle for header drawer.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,3 +3,14 @@ import '../styles/styles.css';
 if (module.hot) {
   module.hot.accept();
 }
+
+// Show & hide header drawer
+
+const headerDrawerTrigger = document.querySelector('.js-headerDrawerTrigger');
+const header = document.querySelector('.js-header');
+
+function handleHeaderDrawerToggle() {
+  header.classList.toggle('drawer-is-visible');
+}
+
+headerDrawerTrigger.addEventListener('click', handleHeaderDrawerToggle);
