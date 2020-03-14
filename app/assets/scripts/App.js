209eb533d83f531b6c39d04e7a1b7dc977ac465e
Ahmed <ahmednhosna@gmail.com>
2020-03-14 16:37:54 +0200

    Completed Reveal on Scroll JS  effects.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,11 @@
 import '../styles/styles.css';
+import HeaderDrawerMenu from './modules/HeaderDrawerMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 if (module.hot) {
   module.hot.accept();
 }
 
-// Show & hide header drawer
-
-const headerDrawerTrigger = document.querySelector('.js-headerDrawerTrigger');
-const header = document.querySelector('.js-header');
-
-function handleHeaderDrawerToggle() {
-  header.classList.toggle('drawer-is-visible');
-}
-
-headerDrawerTrigger.addEventListener('click', handleHeaderDrawerToggle);
+new HeaderDrawerMenu();
+new RevealOnScroll(document.querySelectorAll('.c-feature'), 25);
+new RevealOnScroll(document.querySelectorAll('.c-testimonial'), 25);
